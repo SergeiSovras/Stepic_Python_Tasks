d8e3fd2b3b96e3f0ef52f0d9ad8dada4f74ecb78
serge <sergei.sovras@gmail.com>
2023-02-14 15:06:37 +0300

    String_methods_v3

---
+++ Chart_9_5_String_methods_3/Task_6.py
@@ -0,0 +1,6 @@
+first_name = 'Timur'
+last_name = 'Guev'
+age = 27
+profession = 'math teacher'
+affiliation = 'BeeGeek'
+print(f'Hello, {first_name} {last_name}. You are {age}. You are a {profession}. You were a member of {affiliation}')
